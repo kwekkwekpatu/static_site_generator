fb13322204ec0dd638ab1a7684ec34f6795831dd
kwekkwekpatu <guiljoze@gmail.com>
2024-04-17 20:04:35 +0200

    added some basic python backend

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from textnode import TextNode
+
+def main():
+    test_node = TextNode("This is a test node", "bold", "https://www.isaykonter.com")
+    print(test_node)
+
+if __name__ == "__main__":
+    main()    
