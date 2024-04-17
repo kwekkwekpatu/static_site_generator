1a12e6bfe4498207c3cacbd4af69c9923d1eab34
kwekkwekpatu <guiljoze@gmail.com>
2024-04-17 21:21:00 +0200

    added leafnodes and parentnodes + tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -19,3 +19,40 @@ class HTMLNode:
         return ""
     def __repr__(self) -> str:
         return f"HTMLNODE({self.tag}, {self.value}, children: {self.children}, {self.props})"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None,value=None, props=None) -> None:
+        if value is None:
+            return ValueError("All leafnodes require a value")
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            return ValueError("Invalid HTML: no value")
+        if self.tag is None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.value})"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None) -> None:
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode must have a tag.")
+        if self.children is None:
+            raise ValueError("ParentNode must have children.")
+        to_html_string = ""
+        for child in self.children:
+            to_html_string += child.to_html()
+        to_html_string = f"<{self.tag}{self.props_to_html()}>{to_html_string}</{self.tag}>"
+
+        return to_html_string
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
+    
